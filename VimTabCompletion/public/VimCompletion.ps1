a840eb2d5f7dce2289c74226dbe7b8193af7aec9
John D. Fisher <jdfenw@gmail.com>
2020-07-04 08:51:27 -0700

    feat(VimCompletion): add --remote <files>
    Add Get-VimChildItem.
    
    Add --remote <files> to call Get-VimChildItem.

--- VimTabCompletion/public/VimCompletion.ps1
+++ VimTabCompletion/public/VimCompletion.ps1
@@ -1,30 +1,63 @@
 <#
-    .SYNOPSIS
-    A brief description of this function.
+.SYNOPSIS
+    Provide PowerShell completion for Vim Native applications
 
-    .DESCRIPTION
-    A detailed description of this function.
+.DESCRIPTION
+     Provide completion for vim, vimdiff, gvim, gvimdiff, and evim similar to
+    /usr/share/zsh/functions/Completion/Unix/_vim in zsh.
 
-    .PARAMETER wordToComplete
-    A description of a parameter.
+.PARAMETER wordToComplete
+    This parameter is set to value the user has provided before they pressed
+    Tab. Used to determine tab completion values.
 
-    .PARAMETER commandAst
-    A description of a parameter.
+.PARAMETER commandAst
+    This parameter is set to the Abstract Syntax Tree (AST) for the current
+    input line.
 
-    .PARAMETER cursorPosition
-    A description of a parameter.
+    CommandElements    : {vim, --remote, -}
+    InvocationOperator : Unknown
+    DefiningKeyword    :
+    Redirections       : {}
+    Extent             : vim --remote -
+    Parent             : vim --remote -
 
-    .EXAMPLE
-    An example of this function's usage.
+.PARAMETER cursorPosition
+    This parameter is set to the position of the cursor when the user pressed
+    Tab.
+
+.EXAMPLE
+    PS C:\> vim - <Ctrl-Space>
+    --                    -D                    --literal             -O                    --remote-tab          -T                    -x
+    -A                    -e                    -m                    -p                    --remote-wait         --ttyfail             -y
+    -b                    -E                    -M                    -r                    --remote-wait-silent  -u                    -Z
+    -c                    -h                    -n                    -R                    -s                    -v
+    -C                    -H                    -N                    --remote              -S                    -V
+    --clean               -i                    --noplugin            --remote-expr         --serverlist          --version
+    --cmd                 -l                    --not-a-term          --remote-send         --servername          -w
+    -d                    -L                    -o                    --remote-silent       --startuptime         -W
+
+    --                      Only file names after this
+
+.LINK
+    https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters
+
+.LINK
+    https://docs.microsoft.com/en-us/dotnet/api/system.management.automation.language.ast
 #>
 
 function VimCompletion {
     param($wordToComplete, $commandAst, $cursorPosition)
 
-    Get-VimArguments $wordToComplete $commandAst $cursorPosition |
-        Where-Object { $_.Argument -clike "$wordToComplete*" } |
-        Sort-Object -Property Argument -Unique -CaseSensitive |
-        ForEach-Object -Process {
+    $global:dude = $commandAst
+
+    if ($commandAst.CommandElements[-1].Value -match "^--remote$") {
+        Get-VimChildItem
+        return
+    }
+    Get-VimArguments |
+    Where-Object { $_.Argument -clike "$wordToComplete*" } |
+    Sort-Object -Property Argument -Unique -CaseSensitive |
+    ForEach-Object -Process {
         $completionText = $_.Argument
         $listItemText = $_.Argument
 
@@ -37,6 +70,8 @@ function VimCompletion {
 
         New-Object System.Management.Automation.CompletionResult `
             $completionText, $listItemText, 'ParameterValue', $_.ToolTip
+        # New-TabItem -CompletionText $completionText -ListItemText $listItemText `
+        #     -ResultType ParameterName -ToolTip $_.ToolTip
     }
 }
 
